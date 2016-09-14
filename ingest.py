0db3c529790fd4104165854a2ac82dc8a545ddbd
pcgeller <pcgeller@gmail.com>
2016-09-14 08:36:08 -0400

    read in data from .csv to dataframe

---
+++ ingest.py
@@ -0,0 +1,35 @@
+#Load the data into Python so it can be turned into a
+#bunch object
+import csv
+import sys
+import pandas as pd
+# with open('./data/mors.csv','rb') as f:
+#     reader = csv.reader(f)
+#     try:
+#         for row in reader:
+#             print(row)
+#     except csv.Error as e:
+#         sys.exit('File %s, line %d: %s' % (filename, reader.line_num, e))
+
+#!!!! File encoding problems.  KLUDGE!
+
+#filename = './data/mors8.csv'
+reader = csv.reader(open('./data/mors8.csv','r'))
+for row in reader:
+    print(row)
+
+with open('./data/mors8.csv', 'r') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+
+data = pd.read_csv('./data/mors8.csv')
+headers = list(data.columns.values)
+
+
+# filename )= './data/mors.csv'
+# reader =' csv.reader(open(filename, 'rb'))
+# try:
+#     for row in reader:
+#         print(row)
+#     except csv.Error as e:
+#         sys.exit('file %s, line %d: %s' % (filename, reader.line_num,e))
