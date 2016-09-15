e9498d42100e77a64a45ef364303886c403d20b0
pcgeller <pcgeller@gmail.com>
2016-09-15 07:10:22 -0400

    run through of multiple claclassifiers

--- model.py
+++ model.py
@@ -2,7 +2,9 @@
 #Take munch and create train and test subset - MOVED to WRANGLE
 #Fit model
 #Test model
+import os.path
 import time
+import pickle as pkl
 from analysis import *
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -11,16 +13,22 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn import metrics
 from sklearn import cross_validation
 from sklearn.cross_validation import KFold
+from sklearn.metrics import f1_score
 
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
 
 from sklearn.pipeline import Pipeline
 #Tokenize
-countVec = CountVectorizer()
+countVec = CountVectorizer(stop_words = 'english', ngram_range=(1,3))
 trCount = countVec.fit_transform(train.text)
+
+fullCount = countVec.fit_transform(dataset.text)
+fullTrans = TfidfTransformer(use_idf=False).fit(fullCount)
+fullTF = fullTrans.transform(fullCount)
 
 #Convert tokenized occurances to frequencies
 tfTrans = TfidfTransformer(use_idf=False).fit(trCount)
@@ -29,13 +37,15 @@ trTF = tfTrans.transform(trCount)
 tfidfTrans = TfidfTransformer()
 trTFIDF = tfidfTrans.fit_transform(trCount)
 
+fullTFIDF = tfidfTrans.fit_transform(fullCount)
+
 #Model Naive Bayes
 clf = MultinomialNB().fit(trTFIDF, train.target)
 test = ['The military is using data science']
 testCount = countVec.transform(test)
 testTFIDF = tfidfTrans.transform(testCount)
 
-predicted = clf.predict(testTFIDF)
+# predicted = clf.predict(test)
 
 ##build pipeline
 text_clf = Pipeline([('vect',CountVectorizer()),
@@ -44,13 +54,15 @@ text_clf = Pipeline([('vect',CountVectorizer()),
 ])
 #Test Set Performance
 
+dataset.features = fullTFIDF
+dataset.target = np.asarray(dataset.target)
 def fit_and_evaluate(dataset, model, label, **kwargs):
     '''Function to run multiple models easily.'''
     start = time.time()
     scores = {'precision':[], 'recall':[], 'accuracy':[], 'f1':[]}
 
-    for train, test in KFold(dataset.data.shape[0], n_folds=12, shuffle=True):
-        Xtrain, Xtest = dataset.data[train], dataset.data[test]
+    for train, test in KFold(dataset.features.shape[0], n_folds=12, shuffle=True):
+        Xtrain, Xtest = dataset.features[train], dataset.features[test]
         Ytrain, Ytest = dataset.target[train], dataset.target[test]
 
         estimator = model(**kwargs)
@@ -58,6 +70,7 @@ def fit_and_evaluate(dataset, model, label, **kwargs):
 
         expected = Ytest
         predicted = estimator.predict(Xtest)
+        #print(predicted)
 
         #Save scores in dictionary
         scores['precision'].append(metrics.precision_score(expected, predicted, average="weighted"))
@@ -66,20 +79,26 @@ def fit_and_evaluate(dataset, model, label, **kwargs):
         scores['f1'].append(metrics.f1_score(expected, predicted, average="weighted"))
 
     # Report
-    print "Build and Validation of {} took {:0.3f} seconds".format(label, time.time()-start)
-    print "Validation scores are as follows:\n"
-    print pd.DataFrame(scores).mean()
+    #print("Build and Validation of {} took {:0.3f} seconds".format(label, time.time()-start))
+    print("Validation scores are as follows:\n")
+    print(pd.DataFrame(scores).mean())
 
     #Write the trained estimator to disc
     estimator = model(**kwargs)
-    estimator.fit(dataset.data, dataset.target)
+    estimator.fit(dataset.features, dataset.target)
 
     outpath = label.lower().replace(" ", "-") + ".pickle"
-    with open(outpath, 'w') as f:
-        pickle.dump(estimator, f)
+    with open(outpath, 'wb') as f:
+        pkl.dump(estimator, f)
 
-    print "\nFitted model written to:\n{}".format(os.path.abspath(outpath))
+    print("\nFitted model written to:\n{}".format(os.path.abspath(outpath)))
 
 def decoder(codedarray, le = le):
     decoded = le.inverse_transform(codedarray)
     return(list(decoded))
+
+fit_and_evaluate(dataset,SVC,"Support Vector")
+fit_and_evaluate(dataset, KNeighborsClassifier,"KNN", n_neighbors=12)
+fit_and_evaluate(dataset, RandomForestClassifier, "Random Forest")
+fit_and_evaluate(dataset, SGDClassifier, "SGD", loss = 'hinge', penalty = 'l2',alpha=1e-3, n_iter=5, random_state=4)
+fit_and_evaluate(dataset, MultinomialNB, "Naive Bayes")
